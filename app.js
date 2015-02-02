bdafb7715caab325fa8bb0c61c8eb67ba2c530f1
artur <chelaru.artur@gmail.com>
2015-02-02 02:04:00 +0200

    used spooky.js

--- app.js
+++ app.js
@@ -1,64 +1,20 @@
- var MongoClient = require('mongodb').MongoClient;
+
  var async = require('async');
  var childProcess = require('child_process');
  var fs = require('fs');
  var links = [];
 
- function addProduct(product) {
 
- 	MongoClient.connect('mongodb://127.0.0.1:27017/AmazonScraper', function(err, db) {
- 		if (err) throw err;
- 		var collection = db.collection('cosmetics');
- 		collection.insert(product, function(err, docs) {
- 			console.log('Added one product');
- 			db.close();
- 		});
- 	});
- }
-
- function IsJsonString(str) {
- 	try {
- 		JSON.parse(str);
- 	} catch (e) {
- 		return false;
- 	}
- 	return true;
- }
-
- var q = async.queue(function(l, callback) {
- 	var converted = ''; // convert to unicode code needed to pass arguments to scraper
- 	for (var i = 0; i < l.length; i++) {
- 		converted += l.charCodeAt(i).toString() + '_';
- 	}
-
- 	MongoClient.connect('mongodb://127.0.0.1:27017/AmazonScraper', function(err, db) {
- 		if (err) throw err;
- 		var collection = db.collection('cosmetics');
-
- 		var s = (l) ? 'casperjs scraper.js' + ' ' + converted : 'casperjs scraper.js';
- 		var scraper = childProcess.exec(s, function(error, stdout, stderr) {
- 			//console.log(stdout);
- 		});
-
- 		scraper.stdout.on('data', function(buf) {
- 			if (IsJsonString(buf)) {
- 				collection.insert(JSON.parse(buf), function(err, docs) {
- 					console.log('Added one product');
-
- 				});
- 			}
- 		});
+ var q = async.queue(function(url, callback) {
+ 
+ 	var scraper = childProcess.fork('./scraper.js',[url]);
 
  		scraper.on('exit', function(code) {
  			console.log('Exit with code ', code);
- 			db.close();
  			callback();
  		});
 
- 	});
-
-
- }, 5);
+ }, 4);
 
  /*  Uncoment when you want to get all product page links
   var child = childProcess.fork('./getLinks.js'); //first get links of all pages with list of products
@@ -84,3 +40,4 @@
  		});
  	});
  });
+ 
--- scraper.js
+++ scraper.js
@@ -1,65 +1,101 @@
-var fs = require('fs');
-var args = require('system').args;
-
-var casper = require('casper').create({
-	pageSettings: {
-		loadImages: false, // The WebPage instance used by Casper will
-		loadPlugins: false, // use these settings		
-	}
-});
-var data = [];
-var nextPage = '';
-var t = new Date();
-var next = './nextpage.txt';
-//js code that will be injectet into page and will return product data
-function getProductInfo() {
-	var data = {};
-	data.title = (document.getElementById('productTitle')) ? document.getElementById('productTitle').innerText : null;
-	data.salePrice = (document.getElementById('priceblock_saleprice')) ? document.getElementById('priceblock_saleprice').innerText : null;
-	data.ourPrice = (document.getElementById('priceblock_ourprice')) ? document.getElementById('priceblock_ourprice').innerText : null;
-	data.availability = (document.getElementById('availability')) ? document.getElementById('availability').innerText : null;
-	data.feature_bullets = (document.getElementById('feature-bullets')) ? document.getElementById('feature-bullets').innerText : null;
-	data.description = (document.querySelector('.productDescriptionWrapper')) ? document.querySelector('.productDescriptionWrapper').innerText : null;
-	//get product details 
-	data.productDetails = {};
-	var details = (document.getElementById('detail-bullets')) ? document.getElementById('detail-bullets').querySelectorAll('.content li') : null;
-	if (details !== null) {
-		for (var i = 0; i < details.length; i++) {
-			var key = details[i].querySelector('b').innerText;
-			var val = details[i].innerText;
-			val = val.slice(key.length);
-			data.productDetails[key] = val;
-		}
-	}
-	return data;
+try {
+	var Spooky = require('spooky');
+} catch (e) {
+	var Spooky = require('../lib/spooky');
 }
 
-var url = '';
-var arg = args[args.length - 1];
+var MongoClient = require('mongodb').MongoClient;
 
-arg = arg.split('_');
-arg.pop();
-url = String.fromCharCode.apply(this, arg);
 
-casper.start(url);
-casper.then(function() {
-	var links = [];
-	links = this.evaluate(function() {
-		var links = document.querySelectorAll('.s-access-detail-page');
-		return Array.prototype.map.call(links, function(e) {
-			return e.getAttribute('href');
-		});
-	});
+var url = process.argv[process.argv.length - 1];
+console.log(url);
 
-	//open all products from page and get product data
-	casper.eachThen(links, function(link) {
-		this.thenOpen(link.data, function() {
-			var product = casper.evaluate(getProductInfo);
-			if (product !== null) {
-				console.log(JSON.stringify(product)); // send product to stdout
+MongoClient.connect('mongodb://127.0.0.1:27017/AmazonScraper', function(err, db) {
+	if (err) throw err;
+	var collection = db.collection('cosmetics');
+
+	var spooky = new Spooky({
+		child: {
+			transport: 'http'
+		},
+		casper: {
+			logLevel: 'debug',
+			verbose: true,
+			pageSettings: {
+				loadImages: false, // The WebPage instance used by Casper will
+				loadPlugins: false, // use these settings		
 			}
+		}
+	}, function(err) {
+		if (err) {
+			e = new Error('Failed to initialize SpookyJS');
+			e.details = err;
+			throw e;
+		}
+
+		spooky.start(url);
+		spooky.then(function() {
+
+			var links = [];
+			links = this.evaluate(function() {
+				var links = document.querySelectorAll('.s-access-detail-page');
+				return Array.prototype.map.call(links, function(e) {
+					return e.getAttribute('href');
+				});
+			});
+
+			//open all products from page and get product data
+			this.eachThen(links, function(link) {
+				this.thenOpen(link.data, function() {
+					var product = this.evaluate(function() {
+						var data = {};
+						data.title = (document.getElementById('productTitle')) ? document.getElementById('productTitle').innerText : null;
+						data.salePrice = (document.getElementById('priceblock_saleprice')) ? document.getElementById('priceblock_saleprice').innerText : null;
+						data.ourPrice = (document.getElementById('priceblock_ourprice')) ? document.getElementById('priceblock_ourprice').innerText : null;
+						data.availability = (document.getElementById('availability')) ? document.getElementById('availability').innerText : null;
+						data.feature_bullets = (document.getElementById('feature-bullets')) ? document.getElementById('feature-bullets').innerText : null;
+						data.description = (document.querySelector('.productDescriptionWrapper')) ? document.querySelector('.productDescriptionWrapper').innerText : null;
+						//get product details 
+						data.productDetails = {};
+						var details = (document.getElementById('detail-bullets')) ? document.getElementById('detail-bullets').querySelectorAll('.content li') : null;
+						if (details !== null) {
+							for (var i = 0; i < details.length; i++) {
+								var key = details[i].querySelector('b').innerText;
+								var val = details[i].innerText;
+								val = val.slice(key.length);
+								data.productDetails[key] = val;
+							}
+						}
+						return data;
+					});
+					if (product !== null) {
+						this.emit('product', product);
+					}
+				});
+			});
+		});
+		spooky.run();
+	});
+
+
+	spooky.on('error', function(e, stack) {
+		console.error(e);
+
+		if (stack) {
+			console.log(stack);
+		}
+	});
+
+
+	spooky.on('product', function(product) {
+		collection.insert(product, function(err, docs) {
+			console.log('Added one product');
 		});
 	});
-});
+	spooky.on('exit', function() {
+		db.close();
+		process.exit(0);
+	});
 
-casper.run();
+
+});
