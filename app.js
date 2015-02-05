c89c5f4cc411dfc4bc4e7d899014b9cc0e5d0fca
chelaru-artur <chelaru.artur@mail.ru>
2015-02-05 16:35:42 -0500

    should work

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
  var childProcess = require('child_process');
  var fs = require('fs');
  var links = [];
-
-
+ var toParse =  [];
+ var initialLength = 0;
+ var currentLength = 0;
+/*
  var q = async.queue(function(url, callback) {
  
  	var scraper = childProcess.fork('./scraper.js',[url]);
@@ -15,29 +17,58 @@
  		});
 
  }, 4);
+*/
+ 
+function parseData(url,callback){
+	var scraper = childProcess.spawn('node', ['scraper.js',url],{cwd : process.cwd()});
+scraper.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
 
- /*  Uncoment when you want to get all product page links
+
+                scraper.on('exit', function(code) {
+                        console.log('Exit with code ', code);
+                        callback();
+                });
+
+
+}
+
+function doTheJob(){
+  if(!toParse.length) return;
+  var url = toParse.splice(0,1);
+  parseData(url, function(){
+    currentLength++;
+    if(currentLength === initialLength){
+      // all data parsed
+	console.log("Finish");
+    }
+    doTheJob();
+  });
+};
+/*
   var child = childProcess.fork('./getLinks.js'); //first get links of all pages with list of products
   child.on('exit', function() {
   	var pages = fs.readdirSync('./pages');
   	pages.forEach(function(f) {
-  		fs.readFile('./pages/' + f, 'utf8', function(err, data) {
-  			var data = JSON.parse(data);
-  			data.forEach(function(url) {
-  				q.push(url);
-  			});
-  		});
+  		var data = fs.readFileSync('./pages/' + f, 'utf8')
+  			 data = JSON.parse(data);
+  			 toParse = toParse.concat(data);
   	});
-  });
- */
+	initialLength = toParse.length;
+  	for(var i = 0; i<2; i++)
+    		doTheJob();
 
- var pages = fs.readdirSync('./pages');
- pages.forEach(function(f) {
- 	fs.readFile('./pages/' + f, 'utf8', function(err, data) {
- 		var data = JSON.parse(data);
- 		data.forEach(function(url) {
- 			q.push(url);
- 		});
- 	});
- });
+  });
  
+*/
+
+  var pages = fs.readdirSync('./pages');
+        pages.forEach(function(f) {
+                var data = fs.readFileSync('./pages/' + f, 'utf8')
+                         data = JSON.parse(data);
+                         toParse = toParse.concat(data);
+        });
+        initialLength = toParse.length;
+        for(var i = 0; i<1; i++)
+                doTheJob();
